a62304dc6e9ef7000d6d353992a1f26c70e14f56
unknown <nico.fcok@gmail.com>
2023-07-05 21:29:49 -0300

    se agrega hook form en register y login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,30 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NavbarLanding from '../NavbarLanding/NavbarLanding'
 import Footer from '../Footer/Footer'
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
 
-  const [login, setLogin] = useState({})
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setLogin({
-      ...login,
-      [name]:value
-    })
-  }
+  const URL = 'http://localhost:8000/userlogin'
+  const { register, handleSubmit } = useForm();
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    // console.log(login);
-    const {data} = await axios.post('http://localhost:8000/userlogin', login); 
-
-    if (await data.result.email === login.email) {
-      console.log('OK');
-    } else {
-      console.log('NA');
-    }
+  const onLogin = async (data) => {
+    const respuesta = await axios.post(URL, data);
+    console.log(respuesta.data);
 
   }
 
@@ -32,17 +19,17 @@ const Login = () => {
   return (
     <>
      <NavbarLanding title='Bar RC' linkUno={'Sanguches'} linkDos={'Burgers'} linkTres={'Bebidas'} linkCuatro={'Ingresar'} linkCinco={'Registrarse'} />
-    <main>
+    <main className='min-vh-100'>
       <div className="container">
         <h1>Ingresar</h1>
-        <form onSubmit={onLogin} className='w-50 mx-auto mt-5'>
+        <form onSubmit={handleSubmit(onLogin)} className='w-50 mx-auto mt-5'>
           <div className="mb-3 justify-content-center align-items-center">
             <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Ingresa tu email' name='email' onChange={handleChange}/>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Ingresa tu email' name='email' {...register("email")}/>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" name='password' onChange={handleChange}/>
+            <input type="password" className="form-control" id="exampleInputPassword1" name='password' {...register("password")}/>
           </div>
           <button type="submit" className="btn btn-dark w-50 d-block mx-auto mx-md-0 ms-md-auto">Ingresar</button>
        </form>
--- src/Components/Registro/Registro.js
+++ src/Components/Registro/Registro.js
@@ -1,55 +1,44 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NavbarLanding from '../NavbarLanding/NavbarLanding'
 import Footer from '../Footer/Footer'
 import axios from 'axios'
+import { useForm } from "react-hook-form";
 
 const Registro = () => {
 
-    const [register, setRegister] = useState({})
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setRegister({
-      ...register,
-      estado: 'pendiente',
-      rol: 'user',
-      [name]:value
-    })
-  }
+  const URL = 'http://localhost:8000/post-user'
+  const { register, handleSubmit } = useForm();
 
-  const onRegister = async () => {
-    console.log(register)
+  const onSubmit = async (data) => {
     let confirma = window.confirm('Desea Registrarse?');
     if (confirma) {
-      await axios.post('http://localhost:8000/post-user', register);
+      await axios.post(URL, data);
       alert('Usuario registrado!')
       window.location = 'http://localhost:3000/login'
     }
   }
-
   return (
     <>
      <NavbarLanding title='Bar RC' linkUno={'Sanguches'} linkDos={'Burgers'} linkTres={'Bebidas'} linkCuatro={'Ingresar'} linkCinco={'Registrarse'} />
-    <main className='min-vh-100'>
-      <div className="container">
-        <h1>Registro</h1>
-        <form className='w-50 mx-auto mt-5'>
-          <div className="mb-3 justify-content-center align-items-center">
-            <label htmlFor="lbl-name" className="form-label">Nombre</label>
-            <input type="text" className="form-control" id="lbl-name"  placeholder='Ingresa tu nombre' name='nombre' onChange={handleChange} required/>
-          </div>
-          <div className="mb-3 justify-content-center align-items-center">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Ingresa tu email' name='email' onChange={handleChange} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" name='password' onChange={handleChange} required/>
-          </div>
-          <button type="button" className="btn btn-dark w-50 d-block mx-auto mx-md-0 ms-md-auto" onClick={onRegister}>Registrarse</button>
-          {/* <button type="submit" className="btn btn-dark w-50 d-block mx-auto mx-md-0 ms-md-auto" >Registrarse</button> */}
-       </form>
-      </div>       
+      <main className='min-vh-100'>
+        <div className="container">
+          <h1>Registro</h1>
+          <form className='w-50 mx-auto mt-5' onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3 justify-content-center align-items-center">
+              <label htmlFor="lbl-name" className="form-label">Nombre</label>
+              <input type="text" className="form-control" id="lbl-name"  placeholder='Ingresa tu nombre' name='nombre' {...register("nombre")} required />
+            </div>
+            <div className="mb-3 justify-content-center align-items-center">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Ingresa tu email' name='email' {...register("email")} required/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" name='password' {...register("password")} required/>
+            </div>
+            <button type="submit" className="btn btn-dark w-50 d-block mx-auto mx-md-0 ms-md-auto" >Registrarse</button>
+          </form>
+        </div>       
       </main>
       <Footer styles={'position-absolute bottom-0 w-100'} />
     </>
